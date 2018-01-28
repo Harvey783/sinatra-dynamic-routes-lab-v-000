1c31c42696c133c11e307f7c8288ac1fe59ac750
Chris Harvey <chharvey@icloud.com>
2018-01-28 20:12:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,10 +14,26 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    @number = params[:number].to_i
-    @phrase = params[:phrase]
-    output = @phrase * @number
-    output.to_s
+    final_string = ""
+    (params[:number].to_i).times do
+      final_string += "#{params[:phrase]}\n"
+    end
+    final_string
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @sentencearr = [params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]]
+    @sentence = @sentencearr.join(' ') << '.'
+    @sentence
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+
+    @it = @num1.send(@operation, @num2)
+    @it
   end
 
 end
