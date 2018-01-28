ee5134f94775cd60a94797d09622b804719eb923
Chris Harvey <chharvey@icloud.com>
2018-01-28 20:14:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,12 +28,21 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    @operation = params[:operation]
-    @num1 = params[:number1].to_i
-    @num2 = params[:number2].to_i
+    operation = params[:operation]
+    num1, num2 = params[:number1].to_i, params[:number2].to_i
 
-    @it = @num1.send(@operation, @num2)
-    @it
+    if operation == "add"
+      answer = num1 + num2
+    elsif operation == "subtract"
+      answer = num1 - num2
+    elsif operation == "multiply"
+      answer = num1 * num2
+    elsif operation == "divide"
+      answer = num1 / num2
+    else
+      answer = "Unable to perform this operation."
+    end
+    answer.to_s
   end
 
 end
